a17532c4822aaadd49621d5c91856620fdec47b1
Taha Emre Sevgilice <153989336+tahaemresevgilice@users.noreply.github.com>
2024-06-22 15:21:00 +0300

    Reactive Froms İşlemleri

--- shop/src/app/app-routing.module.ts
+++ shop/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
@@ -5,7 +6,7 @@ import { ProductAddForms1Component } from './product/product-add-forms-1/product
 
 const routes: Routes = [
   {path:'products-add-1',component: ProductAddForms1Component},
-  {path:'products-add-2',component: ProductComponent},
+  {path:'products-add-2',component: ProductAddForms2Component},
   {path:'products',component: ProductComponent},
   {path:'',redirectTo:'products',pathMatch:'full'},
   {path:'products/category/:categoryId',component:ProductComponent}
--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -10,6 +10,7 @@ import { CategotyComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { ProductAddForms1Component } from './product/product-add-forms-1/product-add-forms-1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,14 @@ import { ProductAddForms1Component } from './product/product-add-forms-1/product
     CategotyComponent,
     ProductComponent,
     ProductFilterPipe,
-    ProductAddForms1Component
+    ProductAddForms1Component,
+    ProductAddForms2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [
